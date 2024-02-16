107345291f4aedc85eeb42b2c5b5829820e5653b
Beringer Dorottya <beringerdorottya@gmail.com>
2024-02-16 15:55:49 +0100

    basic setup: mongoose, onchange

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+require('dotenv').config();
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
 
 var app = express();
 
+//basic configuration
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -11,10 +15,14 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3300;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(urlencodedParser);
+
+mongoose.connect(process.env.MONGO_URI);
+
+//app functions start here
